0b5149a0bf7d523eaf977fb3176f361a382e4d49
Kevin Eifinger <k.eifinger@googlemail.com>
2021-09-12 13:44:49 +0200

    fix: Set min ha version 2021.5.0

--- custom_components/here_weather/const.py
+++ custom_components/here_weather/const.py
@@ -17,7 +17,7 @@ from homeassistant.const import (
 
 NAME = "HERE Destination Weather"
 DOMAIN = "here_weather"
-VERSION = "1.0.2"
+VERSION = "1.0.3"
 ISSUE_URL = "https://github.com/eifinger/hass-here-weather/issues"
 
 STARTUP_MESSAGE = f"""
