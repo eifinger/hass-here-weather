7036da903e672368630160fbb4264ad52881d2b7
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2022-12-30 10:33:57 +0100

    fix: Update supported languages
    BREAKING CHANGE: If you configured a language you have to to so again.

--- tests/test_init.py
+++ tests/test_init.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-from custom_components.here_weather.const import DOMAIN
+from custom_components.here_weather.const import CONF_LANGUAGE, DOMAIN
 
 from . import mock_weather_for_coordinates
 from .const import MOCK_CONFIG
@@ -26,3 +26,23 @@ async def test_unload_entry(hass):
         assert await hass.config_entries.async_unload(entry.entry_id)
         await hass.async_block_till_done()
         assert not hass.data[DOMAIN]
+
+
+async def test_migrate_entry_v1(hass, caplog):
+    """Test language migration."""
+    with patch(
+        "aiohere.AioHere.weather_for_coordinates",
+        side_effect=mock_weather_for_coordinates,
+    ):
+        entry = MockConfigEntry(
+            domain=DOMAIN, data=MOCK_CONFIG, options={CONF_LANGUAGE: "German"}
+        )
+        entry.add_to_hass(hass)
+        await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+        assert hass.data[DOMAIN]
+
+        assert (
+            "The configured language was reset. Please configure it again."
+            in caplog.text
+        )
